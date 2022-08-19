e2a52f2486cbc0846c8613376332c406ac7d046a
Yiro Yi <yiroyi@live.com>
2022-08-20 01:04:47 +0900

    Creating Kotlin Counter Android App

---
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myfirstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val myTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+
+        btnClickMe.setOnClickListener {
+            timesClicked += 1
+            myTextView.text = timesClicked.toString()
+            Toast.makeText(this, "Hey I'm toast", Toast.LENGTH_LONG).show()
+        }
+    }
+}
